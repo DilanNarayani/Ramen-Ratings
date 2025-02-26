ce4243ad311552b933d71c8c3dab3e06c6d59fc4
DilanNarayani <dnarayani27@students.stab.org>
2025-02-26 14:41:58 -0500

    Add files via upload

--- ramen.js
+++ ramen.js
@@ -1,3 +1,11 @@
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/Ramen%20Ratings.csv"
+
+var brands = getColumn(url,1);
+var varieties = getColumn(url,2);
+var styles = getColumn(url,3)
+var countries = getColumn(url,4)
+var ratings = getColumn(url,5)
+
 function getColumn(url, columnNumber){
     var column = [];
     var table = [];
@@ -22,15 +30,54 @@ function getColumn(url, columnNumber){
          }
          return column;
       }
+      
+// console.log(countries)
 
 
-var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/Ramen%20Ratings.csv"
-var brands = getColumn(url,1);
-var varieties = getColumn(url,2);
-var styles = getColumn(url,3);
-var flavors = getColumn(url,2);
-var countries = getColumn(url,4)
-var ratings = getColumn(url,5)
+/*
+The purpose of this is to input a country and return a list of all the flavors for that country. 
+country {string} - The desired country
+return {list} - A list of flavors in that country output
+*/
+
+function getVarieties (country){
+    var matches = []
+    for(var i = 0; i < brands.length; i++){
+        if(countries[i].toLowerCase().includes(country.toLowerCase())){
+            matches.push(varieties[i])
+        }
+    }
+    if(matches.length == 0){
+        matches.push("No Returns")
+    }
+    return matches;
+}
+
+console.log(getVarieties(""))
+
+
+
+/*
+The purpose of this is to input a brand and return a ratings. 
+brand {string} - The desired brand.
+return {list} - A list of ratings in that brand output
+*/
+
+function getRating (brand){
+    var matches = []
+    for(var i = 0; i < ratings.length; i++){
+        if(brands[i].toLowerCase().includes(brand.toLowerCase())){
+            matches.push(ratings[i])
+        }
+    }
+    if(matches.length == 0){
+        matches.push("No Returns")
+    }
+    return matches;
+}
+
+console.log(getRating(""))
+
 
 function getFlavor(brandname) {
     var flavor = []
@@ -64,8 +111,9 @@ for( var i = 0; i < brands.length; i++){
 }
  console.log(getStyleFlavor(""))
 
-var countries = getColumn(url,4)
-var ratings = getColumn(url,5)
+
+
+
 
  function averageRating(country){
     var rating = 0;
@@ -79,8 +127,6 @@ var ratings = getColumn(url,5)
     if(!exists){
         return "No Matches"
     }
-
-
     for( var i = 0; i < countries.length; i++){
         if (countries[i].toLowerCase().includes(country.toLowerCase())){
             if (ratings[i] != "NR")  {  
@@ -93,4 +139,3 @@ var ratings = getColumn(url,5)
     return rating/count;
  }
 console.log(averageRating(""))
-      
