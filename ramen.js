68634bdf84fbfa8f7766e90e608c2fb61c1232c5
maclaing12345 <mlaing28@students.stab.org>
2025-02-26 14:37:24 -0500

    Update ramen.js

--- ramen.js
+++ ramen.js
@@ -22,4 +22,75 @@ function getColumn(url, columnNumber){
          }
          return column;
       }
+
+
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/Ramen%20Ratings.csv"
+var brands = getColumn(url,1);
+var varieties = getColumn(url,2);
+var styles = getColumn(url,3);
+var flavors = getColumn(url,2);
+var countries = getColumn(url,4)
+var ratings = getColumn(url,5)
+
+function getFlavor(brandname) {
+    var flavor = []
+    for( var i = 0; i < brands.length; i++){  
+        if (brands[i].toLowerCase().includes(brandname.toLowerCase())){
+           flavor.push(varieties[i]);
+        }
+    }
+if (flavor.length == 0 ){
+    flavor.push("No matches")
+}
+    return flavor
+}
+console.log(getFlavor(""))
+
+var url = "https://raw.githubusercontent.com/b-mcavoy/datasets/refs/heads/main/Miscellaneous/Ramen%20Ratings.csv"
+var styles = getColumn(url,3);
+var flavors = getColumn(url,2);
+
+function getStyleFlavor(style){
+ var flavorForStyle = []
+for( var i = 0; i < brands.length; i++){
+     if(styles[i].toLowerCase().includes(style.toLowerCase())){
+     flavorForStyle.push(varieties[i]) 
+    }
+}
+ if (flavorForStyle.length == 0  ){
+  flavorForStyle.push("No matches")
+ }
+ return flavorForStyle 
+}
+ console.log(getStyleFlavor(""))
+
+var countries = getColumn(url,4)
+var ratings = getColumn(url,5)
+
+ function averageRating(country){
+    var rating = 0;
+    var count = 0;
+    var exists = false;
+    for( var i = 0; i < countries.length; i++){
+        if(countries[i].toLowerCase().includes(country.toLowerCase())){
+            exists = true;
+        }
+    }
+    if(!exists){
+        return "No Matches"
+    }
+
+
+    for( var i = 0; i < countries.length; i++){
+        if (countries[i].toLowerCase().includes(country.toLowerCase())){
+            if (ratings[i] != "NR")  {  
+                rating += parseFloat(ratings[i]);
+                count++;
+            }
+        }
+        
+     }
+    return rating/count;
+ }
+console.log(averageRating(""))
       
